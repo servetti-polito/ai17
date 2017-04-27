a1347b2228e21c23b8f623cf1be6f648f7789fd7
Antonio Servetti <antonio.servetti@polito.it>
2017-04-28 00:09:53 +0200

    Modified controller with array and ng-repeat

--- angular-base-example/index.js
+++ angular-base-example/index.js
@@ -1,4 +1,14 @@
 angular.module('app', [])
 .controller('main-controller', function ($scope) {
   $scope.name = "world";
+  $scope.lines = [
+    { 
+    "line": "METRO",
+    "desc": "Linea 1 Metropolitana - stazione Fermi (Collegno) - stazione Lingotto"
+    },
+    {
+    "line": "41",
+    "desc": "circolare, via Pannunzio (Stazione Lingotto) - via Orbassano (Beinasco-Borgaretto)"
+    }
+  ];
 });
